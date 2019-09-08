fd623278552601fd2b4b88bb480b30c4065c5f25
zhaojw <41221705+zhaojw219@users.noreply.github.com>
2019-09-08 21:10:02 +0800

    Update garbage_cls.py

--- garbage_cls.py
+++ garbage_cls.py
@@ -1,14 +1,19 @@
-import os
-import numpy as np 
-import pandas as pd 
-from keras import optimizers
-os.environ['CUDA_VISIBLE_DEVICES']='0'
 
+# coding: utf-8
+
+import os
 from PIL import ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
+import numpy as np 
+import pandas as pd 
+from keras import optimizers
+
+import os
+os.environ['CUDA_VISIBLE_DEVICES']='0'
+
 from keras.applications import Xception
-from keras.callbacks import TensorBoard, ModelCheckpoint, LearningRateScheduler
+from keras.callbacks import TensorBoard, ModelCheckpoint, LearningRateScheduler, ReduceLROnPlateau
 from keras.models import Model
 from keras.layers import Dense, Dropout
 from keras.optimizers import RMSprop
@@ -17,7 +22,8 @@ from keras.regularizers import l2
 
 CATEGORIES = ['harmful_garbage', 'kitchen_garbage', 'other_garbage', 'recyclable']
 
-split=True
+# split validation set
+split=False
 
 if split:
     import random
@@ -26,30 +32,11 @@ if split:
     os.mkdir('./dev/')
     for category in CATEGORIES:
         os.mkdir('./dev/' + category)
-        name = os.listdir('./result/' + category)
+        name = os.listdir('/home/gnss/Desktop/result/' + category)
         random.shuffle(name)
         todev = name[:int(len(name) * .2)]
         for file in todev:
-            shutil.move(os.path.join('./result', category, file), os.path.join('dev', category))
-
-'''
-def lr_schedule(epoch):
-    """Learning Rate Schedule
-    Learning rate is scheduled to be reduced after 80, 120epochs.
-    Called automatically every epoch as part of callbacks during training.
-    # Arguments
-        epoch (int): The number of epochs
-    # Returns
-        lr (float32): learning rate
-    """
-    lr = 1e-4
-    if epoch > 80:
-        lr *= 1e-2
-    elif epoch > 40:
-        lr *= 1e-1
-    print('Learning rate: ', lr)
-    return lr
-'''
+            shutil.move(os.path.join('/home/gnss/Desktop/result', category, file), os.path.join('dev', category))
 
 # data generator 
 train_datagen = ImageDataGenerator(
@@ -81,10 +68,8 @@ val_generator = val_datagen.flow_from_directory(
 
 tensorboard = TensorBoard('./logs')
 
-
-
-basic_model = Xception(include_top=False, weights='imagenet', pooling='avg')
-# basic_model = Xception(include_top=False, pooling='avg')
+# basic_model = Xception(include_top=False, weights='imagenet', pooling='avg')
+basic_model = Xception(include_top=False, pooling='avg')
 
 for layer in basic_model.layers:
     layer.trainable = True
@@ -98,23 +83,23 @@ x = Dense(len(CATEGORIES), activation='softmax')(x)
 
 model = Model(inputs=input_tensor, outputs=x)
 
-'''
 model_path='./logs/weights_xception.h5'
 model.load_weights(model_path, by_name=True)
 print('load weights')
-'''
 
 adam = optimizers.Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
 
 model.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])
 
+checkpointer = ModelCheckpoint(filepath='/home/gnss/Desktop/logs/weights_xception.h5',verbose=1,save_best_only=True)
 
-checkpointer = ModelCheckpoint(filepath='./logs/weights_xception.h5',verbose=1,save_best_only=True)
+reduce_lr = ReduceLROnPlateau(monitor='val_loss',factor=0.1,patience=3,verbose=1)
 
 model.fit_generator(train_generator, steps_per_epoch=1000,epochs=1000, 
                     validation_steps=500,
                     validation_data=val_generator,
-                    callbacks=[checkpointer,tensorboard],
+                    callbacks=[checkpointer,tensorboard,reduce_lr],
+                    initial_epoch=40,
                     workers=4,
                     verbose=1)
 
