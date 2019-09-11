d6be0c091ea21b0c47919914345e90b728fc0406
zhaojw219 <852755528@qq.com>
2019-09-11 22:05:30 +0800

    mobilenet v2

---
+++ train.py
@@ -0,0 +1,380 @@
+"""
+Train the MobileNet V2 model
+"""
+import os
+import sys
+import argparse
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from mobilenet_v2 import MobileNetv2
+
+from keras.optimizers import Adam
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import TensorBoard, ModelCheckpoint, LearningRateScheduler, ReduceLROnPlateau,EarlyStopping
+from keras.layers import Conv2D, Reshape, Activation
+from keras.models import Model
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras import backend as K
+
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+def main(argv):
+    parser = argparse.ArgumentParser()
+    # Required arguments.
+    parser.add_argument(
+        "--classes",
+        help="The number of classes of dataset.")
+    # Optional arguments.
+    parser.add_argument(
+        "--size",
+        default=224,
+        help="The image size of train sample.")
+    parser.add_argument(
+        "--batch",
+        default=32,
+        help="The number of train samples per batch.")
+    parser.add_argument(
+        "--epochs",
+        default=300,
+        help="The number of train iterations.")
+    parser.add_argument(
+        "--weights",
+        default=False,
+        help="Fine tune with other weights.")
+    parser.add_argument(
+        "--tclasses",
+        default=0,
+        help="The number of classes of pre-trained model.")
+
+    args = parser.parse_args()
+
+
+    train(int(args.batch), int(args.epochs), int(args.classes), int(args.size), args.weights, int(args.tclasses))
+
+
+class MixupImageDataGenerator():
+    def __init__(self, generator, directory, batch_size, img_height, img_width, alpha=0.2, subset=None):
+
+        self.batch_index = 0
+        self.batch_size = batch_size
+        self.alpha = alpha
+
+        # First iterator yielding tuples of (x, y)
+        self.generator1 = generator.flow_from_directory(directory,
+                                                        target_size=(
+                                                            img_height, img_width),
+                                                        class_mode="categorical",
+                                                        batch_size=batch_size,
+                                                        shuffle=True,
+                                                        subset=subset)
+
+        # Second iterator yielding tuples of (x, y)
+        self.generator2 = generator.flow_from_directory(directory,
+                                                        target_size=(
+                                                            img_height, img_width),
+                                                        class_mode="categorical",
+                                                        batch_size=batch_size,
+                                                        shuffle=True,
+                                                        subset=subset)
+
+        # Number of images across all classes in image directory.
+        self.n = self.generator1.samples
+
+    def reset_index(self):
+        """Reset the generator indexes array.
+        """
+
+        self.generator1._set_index_array()
+        self.generator2._set_index_array()
+
+    def on_epoch_end(self):
+        self.reset_index()
+
+    def reset(self):
+        self.batch_index = 0
+
+    def __len__(self):
+        # round up
+        return (self.n + self.batch_size - 1) // self.batch_size
+
+    def get_steps_per_epoch(self):
+        """Get number of steps per epoch based on batch size and
+        number of images.
+
+        Returns:
+            int -- steps per epoch.
+        """
+
+        return self.n // self.batch_size
+
+    def __next__(self):
+        """Get next batch input/output pair.
+
+        Returns:
+            tuple -- batch of input/output pair, (inputs, outputs).
+        """
+
+        if self.batch_index == 0:
+            self.reset_index()
+
+        current_index = (self.batch_index * self.batch_size) % self.n
+        if self.n > current_index + self.batch_size:
+            self.batch_index += 1
+        else:
+            self.batch_index = 0
+
+        # random sample the lambda value from beta distribution.
+        l = np.random.beta(self.alpha, self.alpha, self.batch_size)
+
+        X_l = l.reshape(self.batch_size, 1, 1, 1)
+        y_l = l.reshape(self.batch_size, 1)
+
+        # Get a pair of inputs and outputs from two iterators.
+        X1, y1 = self.generator1.next()
+        X2, y2 = self.generator2.next()
+
+        # Perform the mixup.
+        X = X1 * X_l + X2 * (1 - X_l)
+        y = y1 * y_l + y2 * (1 - y_l)
+        return X, y
+
+    def __iter__(self):
+        while True:
+            yield next(self)
+
+
+
+
+def generate(batch, size):
+    """Data generation and augmentation
+
+    # Arguments
+        batch: Integer, batch size.
+        size: Integer, image size.
+
+    # Returns
+        train_generator: train set generator
+        validation_generator: validation set generator
+        count1: Integer, number of train set.
+        count2: Integer, number of test set.
+    """
+
+    #  Using the data Augmentation in traning data
+    ptrain = '/home/gnss/Desktop/garbage_train'
+    pval = '/home/gnss/Desktop/garbage_test'
+
+    datagen1 = ImageDataGenerator(
+        rescale=1. / 255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        rotation_range=90,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        horizontal_flip=True)
+
+    datagen2 = ImageDataGenerator(rescale=1. / 255)
+
+    train_generator = datagen1.flow_from_directory(
+        ptrain,
+        target_size=(size, size),
+        batch_size=batch,
+        class_mode='categorical')
+
+    validation_generator = datagen2.flow_from_directory(
+        pval,
+        target_size=(size, size),
+        batch_size=batch,
+        class_mode='categorical')
+
+    count1 = 0
+    for root, dirs, files in os.walk(ptrain):
+        for each in files:
+            count1 += 1
+
+    count2 = 0
+    for root, dirs, files in os.walk(pval):
+        for each in files:
+            count2 += 1
+
+    return train_generator, validation_generator, count1, count2
+
+
+def fine_tune(num_classes, weights, model):
+    """Re-build model with current num_classes.
+
+    # Arguments
+        num_classes, Integer, The number of classes of dataset.
+        tune, String, The pre_trained model weights.
+        model, Model, The model structure.
+    """
+    model.load_weights(weights)
+
+    x = model.get_layer('Dropout').output
+    x = Conv2D(num_classes, (1, 1), padding='same')(x)
+    x = Activation('softmax', name='softmax')(x)
+    output = Reshape((num_classes,))(x)
+
+    model = Model(inputs=model.input, outputs=output)
+
+    return model
+
+
+
+def cosine_decay_with_warmup(global_step,
+                             learning_rate_base,
+                             total_steps,
+                             warmup_learning_rate=0.0,
+                             warmup_steps=100,
+                             hold_base_rate_steps=0):
+
+    if total_steps < warmup_steps:
+        raise ValueError('total_steps must be larger or equal to '
+                         'warmup_steps.')
+    learning_rate = 0.5 * learning_rate_base * (1 + np.cos(
+        np.pi *
+        (global_step - warmup_steps - hold_base_rate_steps
+         ) / float(total_steps - warmup_steps - hold_base_rate_steps)))
+    if hold_base_rate_steps > 0:
+        learning_rate = np.where(global_step > warmup_steps + hold_base_rate_steps,
+                                 learning_rate, learning_rate_base)
+    if warmup_steps > 0:
+        if learning_rate_base < warmup_learning_rate:
+            raise ValueError('learning_rate_base must be larger or equal to '
+                             'warmup_learning_rate.')
+        slope = (learning_rate_base - warmup_learning_rate) / warmup_steps
+        warmup_rate = slope * global_step + warmup_learning_rate
+        learning_rate = np.where(global_step < warmup_steps, warmup_rate,
+                                 learning_rate)
+    return np.where(global_step > total_steps, 0.0, learning_rate)
+
+
+class WarmUpCosineDecayScheduler(keras.callbacks.Callback):
+    def __init__(self,
+                 learning_rate_base,
+                 total_steps,
+                 global_step_init=0,
+                 warmup_learning_rate=0.0,
+                 warmup_steps=100,
+                 hold_base_rate_steps=0,
+                 verbose=0):
+
+        super(WarmUpCosineDecayScheduler, self).__init__()
+        self.learning_rate_base = learning_rate_base
+        self.total_steps = total_steps
+        self.global_step = global_step_init
+        self.warmup_learning_rate = warmup_learning_rate
+        self.warmup_steps = warmup_steps
+        self.hold_base_rate_steps = hold_base_rate_steps
+        self.verbose = verbose
+        self.learning_rates = []
+
+    def on_batch_end(self, batch, logs=None):
+        self.global_step = self.global_step + 1
+        lr = K.get_value(self.model.optimizer.lr)
+        self.learning_rates.append(lr)
+
+    def on_batch_begin(self, batch, logs=None):
+        lr = cosine_decay_with_warmup(global_step=self.global_step,
+                                      learning_rate_base=self.learning_rate_base,
+                                      total_steps=self.total_steps,
+                                      warmup_learning_rate=self.warmup_learning_rate,
+                                      warmup_steps=self.warmup_steps,
+                                      hold_base_rate_steps=self.hold_base_rate_steps)
+        K.set_value(self.model.optimizer.lr, lr)
+        if self.verbose > 0:
+            print('\nBatch %05d: setting learning '
+                  'rate to %s.' % (self.global_step + 1, lr))
+
+
+def train(batch, epochs, num_classes, size, weights, tclasses):
+    """Train the model.
+
+    # Arguments
+        batch: Integer, The number of train samples per batch.
+        epochs: Integer, The number of train iterations.
+        num_classes, Integer, The number of classes of dataset.
+        size: Integer, image size.
+        weights, String, The pre_trained model weights.
+        tclasses, Integer, The number of classes of pre-trained model.
+    """
+
+    train_generator, validation_generator, count1, count2 = generate(batch, size)
+
+    train_generator = ImageDataGenerator(
+        rescale=1. / 255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        rotation_range=90,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        brightness_range=(1, 1.3),
+        horizontal_flip=True)
+    
+    directory="/home/gnss/Desktop/garbage_train"
+    train_generator = MixupImageDataGenerator(generator=train_generator,
+                                          directory=directory,
+                                          batch_size=32,
+                                          img_height=224,
+                                          img_width=224,
+                                          subset='training')
+
+    if weights:
+        model = MobileNetv2((size, size, 3), tclasses)
+        model = fine_tune(num_classes, weights, model)
+        print(num_classes)
+    else:
+        model = MobileNetv2((size, size, 3), num_classes)
+        print(num_classes)
+
+    opt = Adam(1e-2)
+    # earlystop = EarlyStopping(monitor='val_acc', patience=30, verbose=0, mode='auto')
+    tensorboard = TensorBoard('/home/gnss/Desktop/MobileNetV2/logs',write_images=True)
+    # reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.5,patience=3, verbose=0, mode='auto', epsilon=0.0001, cooldown=0, min_lr=0)
+
+    warmup_epoch = 5
+    
+    warm_up_lr = WarmUpCosineDecayScheduler(learning_rate_base=1e-2,
+                                            total_steps=count1 // batch)
+
+    checkpointer = ModelCheckpoint(filepath='/home/gnss/Desktop/MobileNetV2/mobilenet.h5',verbose=1,save_best_only=True)
+
+    model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+
+    
+
+    # lr=XTensorBoard('/home/gnss/Desktop/MobileNetV2/logslr')
+
+    hist = model.fit_generator(
+        train_generator,
+        validation_data=validation_generator,
+        steps_per_epoch=count1 // batch,
+        validation_steps=count2 // batch,
+        epochs=epochs,
+        callbacks=[warm_up_lr,tensorboard,checkpointer])
+    '''
+    learning_rate_base=1e-2
+    total_steps=count1 // batch
+    plt.plot(warm_up_lr.learning_rates)
+    plt.xlabel('Step', fontsize=20)
+    plt.ylabel('lr', fontsize=20)
+    plt.axis([0, total_steps, 0, learning_rate_base*1.1])
+    plt.xticks(np.arange(0, total_steps, 50))
+    plt.grid()
+    plt.title('Cosine decay with warmup', fontsize=20)
+    plt.show()
+    '''
+    if not os.path.exists('model'):
+        os.makedirs('model')
+
+    df = pd.DataFrame.from_dict(hist.history)
+    df.to_csv('model/hist.csv', encoding='utf-8', index=False)
+    model.save_weights('model/weights.h5')
+
+
+if __name__ == '__main__':
+    main(sys.argv)
